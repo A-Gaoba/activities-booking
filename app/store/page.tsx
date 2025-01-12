3f9fe26812a1283acf704f029a8411ac63136e58
a-gaoba <abdulrahman1997abdo@gmail.com>
2025-01-12 18:04:04 +0300

    final project

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -4,7 +4,7 @@ import Cart from '../components/Cart'
 
 export default function StorePage() {
   return (
-    <div className="container mx-auto px-4 py-8 md:h-[70vh] h-[30vh]">
+    <div className="container mx-auto px-4 py-8 ">
       <h1 className="text-3xl font-bold mb-8 text-center">سلة التسوق</h1>
       <Cart />
     </div>
